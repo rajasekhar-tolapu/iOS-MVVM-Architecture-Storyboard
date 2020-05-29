3d068c126d55e318f0f25923cadce646a7b58af4
Rajasekhar <unknown>
2020-05-30 00:45:19 +0530

    Password screen implemetation

---
+++ Storyboard_MVVM/presentation/forgetPassword/ForgetPasswordViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ForgetPasswordViewModel.swift
+//  Storyboard_MVVM
+//
+//  Created by Rajasekhar on 30/05/20.
+//  Copyright © 2020 Techlabroid. All rights reserved.
+//
+
+import Foundation
+
+class ForgetPasswordViewModel: BaseViewModel {
+    
+}
